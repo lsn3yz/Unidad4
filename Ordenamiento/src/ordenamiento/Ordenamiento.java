9d91372b8482e6ad9283bea559931d1ad02f0b84
lsn3yz <124959988+lsn3yz@users.noreply.github.com>
2023-03-10 11:06:45 -0500

    Add files via upload

---
+++ Ordenamiento/src/ordenamiento/Ordenamiento.java
@@ -0,0 +1,23 @@
+
+package ordenamiento;
+
+import Frames.Main;
+import java.util.Arrays;
+
+/**
+ *
+ * @author sn3yz
+ */
+public class Ordenamiento {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main main = new Main();
+        main.show();
+        
+        
+    }
+}
